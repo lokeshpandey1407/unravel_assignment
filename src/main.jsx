d15fa08dfaace22f479c4e35a4aeb0b74f5c973e
lokeshTDL <lokesh@techdocklabs.com>
2024-09-29 19:56:02 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes.jsx";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
+  <StrictMode>
     <RouterProvider router={router} />
-  // </StrictMode>
+  </StrictMode>
 );
